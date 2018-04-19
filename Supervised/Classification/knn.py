7ccd736c5f882422061108f631e905291b038c3a
Nebula <yrajsm@gmail.com>
2018-04-19 22:01:55 +0900

    Update knn.py

--- Supervised/Classification/knn.py
+++ Supervised/Classification/knn.py
@@ -22,5 +22,24 @@ def pred(Train_X,Train_Y,Test_X,k):
 		dist = np.sum(np.square(Train_X-Test_X[item,:]),axis=1)
 		close_labels = Train_Y[dist.argsort()[:k]] # sorting the indexs of k mimimum distances and value at that index
 		pred_Y[item] = np.bincount(close_labels).argmax() # choosing the most frequent label | counting frequency of each element and then max
-
+		
 	return pred_Y
+
+def performance(k,Test_Y):
+	accuracy = np.zeros(k)
+	for i in xrange(1,k):
+		pred_y = pred(Train_X, Train_Y, Test_X, i) #predicting the label with i neighbour from 1-k
+		accuracy[i-1] = (float(np.sum(np.equal(Test_Y,pred_y)))/30)*100 # finding the number of correct prediction with testY
+
+	return accuracy
+
+ac = performance(k, Test_Y)
+
+#x = np.arange(k)
+plt.plot(ac) # or plt.plot(x,ac) 
+plt.title('Performcace of k-NN with hyperparameter variation')
+plt.ylabel('accuracy %')
+plt.xlabel('k value')
+plt.xlim((1,100))
+plt.grid() 
+plt.show()

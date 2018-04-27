6b15aae9b778f49de795157376db47c01d5cd263
Nebula <yrajsm@gmail.com>
2018-04-27 19:51:05 +0900

    Update lda.py

--- Supervised/Classification/lda.py
+++ Supervised/Classification/lda.py
@@ -1,13 +1,10 @@
 import numpy as np 
 import pandas as pd
 import seaborn as sns
+import matplotlib
 import matplotlib.pyplot as plt
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 
-df = pd.DataFrame()
-df['x1']= [4,2,2,3,4,9,6,9,8,10]
-df['x2']= [1,4,3,6,4,10,8,5,7,8]
-df['class']=[1,1,1,1,1,2,2,2,2,2]
-sns.set()
 
 class1 = np.array([[4, 1], [2, 4],[2, 3],[3, 6],[4, 4]])
 class2 = np.array([[9,10],[6, 8],[9, 5],[8, 7],[10, 8]])
@@ -16,7 +13,7 @@ m1 = np.mean(class1,axis=0)
 m2 = np.mean(class2,axis=0)
 
 scatter = {}
-
+# calculation of scatter within class
 def scattr():
 	l1= np.subtract(class1,m1)
 	l2= np.subtract(class2,m2)
@@ -41,4 +38,55 @@ def weight(scatter):
 	W = np.dot(np.linalg.inv(Sw),m1-m2)
 	return W
 
-W = weight(scatter) 
+W = weight(scatter).reshape((2,1))
+
+#ploting decision boundary
+df = pd.DataFrame()
+df['x1']= [4,2,2,3,4,9,6,9,8,10]
+df['x2']= [1,4,3,6,4,10,8,5,7,8]
+df['class']=[1,1,1,1,1,2,2,2,2,2]
+
+y=np.linspace(-6, 0).reshape((1,50))
+fitx = np.dot(np.linalg.inv(np.dot(W, W.T)),np.dot(W,y)).T
+
+fig = plt.subplots()
+
+sns.set()
+g = sns.lmplot(x="x1", y="x2", hue="class", data=df, fit_reg=False,  scatter_kws={"marker": "D","s": 70})
+sns.regplot(fitx[:,0],fitx[:,1], scatter=False, ax=g.axes[0, 0],ci=None)
+
+#using sklearn
+X = np.array([[4, 1],[2, 4],[2, 3],[3, 6],[4, 4],[9,10],[6, 8],[9, 5],[8, 7],[10, 8]])
+Y = np.array([1,1,1,1,1,2,2,2,2,2])
+clf = LinearDiscriminantAnalysis()
+
+clf.fit(X, Y)
+
+
+h = .02     
+
+# Plot the decision boundary. For that, we will assign a color to each
+x_min, x_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+y_min, y_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+xx, yy = np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min, y_max, h))
+
+# Obtain labels for each point in mesh. Use last trained model.
+Z = clf.predict(np.c_[xx.ravel(), yy.ravel()])
+
+Z = Z.reshape(xx.shape)
+plt.figure(1)
+plt.clf()
+plt.imshow(Z, interpolation='nearest',extent=(xx.min(), xx.max(), yy.min(), yy.max()),
+           cmap=plt.cm.Paired,aspect='auto', origin='lower')
+colors = ['blue','green'] #color for class
+plt.scatter(X[:, 0],X[:, 1], c=Y, cmap=matplotlib.colors.ListedColormap(colors))
+# plt.plot(X[:, 0],X[:, 1], markersize=20)
+plt.xlim(x_min, x_max)
+plt.ylim(y_min, y_max)
+plt.xlabel('x1')
+plt.ylabel('x2')
+plt.xticks(())
+plt.yticks(())
+plt.title('Linear Discriminant Analysis')
+
+plt.show()

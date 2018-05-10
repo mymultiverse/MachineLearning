d547d4c8d34c14aac61fa8b2d33447b9f5661261
Nebula <yrajsm@gmail.com>
2018-05-10 16:39:39 +0900

    Update nbayes.py

--- Supervised/Classification/nbayes.py
+++ Supervised/Classification/nbayes.py
@@ -19,6 +19,9 @@ Setosa = iris.data[:50,:]
 Versicolour = iris.data[50:100,:]
 Virginica = iris.data[100:,:]
 
+fig = plt.figure()
+ax = fig.add_subplot(1, 1, 1, projection='3d')
+
 S_mux01 = np.mean(Setosa[:,:2],axis=0)
 S_sigmax01 = np.std(Setosa[:,:2],axis=0)
 S_cov = np.diag(S_sigmax01**2)
@@ -31,10 +34,6 @@ Ver_cov = np.diag(Ver_sigmax01**2)
 Vir_mux01 = np.mean(Virginica[:,:2],axis=0)
 Vir_sigmax01 = np.std(Virginica[:,:2],axis=0)
 Vir_cov = np.diag(Vir_sigmax01**2)
-
-
-fig = plt.figure()
-ax = fig.add_subplot(1, 1, 1, projection='3d')
 
 x= np.sort(Setosa[:,0])
 y= np.sort(Setosa[:,1])
@@ -68,6 +67,53 @@ xy = np.column_stack([x.flat, y.flat])
 z = multivariate_normal.pdf(xy, mean=Vir_mux01, cov=Vir_cov)
 z = z.reshape(x.shape)
 ax.plot_surface(x,y,z,color='c',label='Vir')
+
+
+# distribution plot for other two features
+# S_mux01 = np.mean(Setosa[:,2:],axis=0)
+# S_sigmax01 = np.std(Setosa[:,2:],axis=0)
+# S_cov = np.diag(S_sigmax01**2)
+
+# Ver_mux01 = np.mean(Versicolour[:,2:],axis=0)
+# Ver_sigmax01 = np.std(Versicolour[:,2:],axis=0)
+# Ver_cov = np.diag(Ver_sigmax01**2)
+
+
+# Vir_mux01 = np.mean(Virginica[:,2:],axis=0)
+# Vir_sigmax01 = np.std(Virginica[:,2:],axis=0)
+# Vir_cov = np.diag(Vir_sigmax01**2)
+
+# x= np.sort(Setosa[:,2])
+# y= np.sort(Setosa[:,3])
+# x, y = np.meshgrid(x,y)
+# # Need an (N, 2) array of (x, y) pairs.
+# xy = np.column_stack([x.flat, y.flat])
+
+# xy = np.column_stack([x.flat, y.flat])
+# z = multivariate_normal.pdf(xy, mean=S_mux01, cov=S_cov)
+# z = z.reshape(x.shape)
+# ax.plot_surface(x,y,z,color='m',label='Setosa')
+
+# x= np.sort(Versicolour[:,2])
+# y= np.sort(Versicolour[:,3])
+# x, y = np.meshgrid(x,y)
+# xy = np.column_stack([x.flat, y.flat])
+
+# xy = np.column_stack([x.flat, y.flat])
+# z = multivariate_normal.pdf(xy, mean=Ver_mux01, cov=Ver_cov)
+# z = z.reshape(x.shape)
+# ax.plot_surface(x,y,z, color='r',label='Ver')
+
+
+# x= np.sort(Virginica[:,2])
+# y= np.sort(Virginica[:,3])
+# x, y = np.meshgrid(x,y)
+# xy = np.column_stack([x.flat, y.flat])
+
+# xy = np.column_stack([x.flat, y.flat])
+# z = multivariate_normal.pdf(xy, mean=Vir_mux01, cov=Vir_cov)
+# z = z.reshape(x.shape)
+# ax.plot_surface(x,y,z,color='c',label='Vir')
 
 plt.xlabel('sepal length')
 plt.ylabel('sepal width')
